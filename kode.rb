b307f92d609eb794a1ca27729357e93d8b6ee94a
AlinaHaha <2ndalina@gmail.com>
2018-08-10 13:24:45 +0000

    automatically backed up by learn

--- kode.rb
+++ kode.rb
@@ -1,9 +1,19 @@
 class Kode
-  def initialize (year, laptop, table)
+  def initialize (year, laptop, students)
     @year = year
     @laptop = laptop
     @table = table
   end
   
   
-  def 
+  def year
+    @year
+  end
+  
+def laptop
+  @laptop
+end
+end
+
+kode_with_klossy = Kode.new("2018", "MacBook Pro", "")
+    
